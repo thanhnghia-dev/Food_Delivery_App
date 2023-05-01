c5a62b592679c720fafcaea5c5210c5b0e09cb50
hothanhnghia-nlu <nghiant369@gmail.com>
2023-05-01 10:09:00 +0700

    Create smooth slide animation

---
+++ app/src/main/java/com/example/food_delivery_app/model/Cart.java
@@ -0,0 +1,67 @@
+package com.example.food_delivery_app.model;
+
+import java.io.Serializable;
+import java.util.HashMap;
+
+public class Cart implements Serializable {
+    private int id;
+    private HashMap<String, Product> productList;
+    private User customer;
+    private double totalMoney;
+    private int quantity;
+
+    public Cart() {
+        productList = new HashMap<>();
+        customer = new User();
+        totalMoney = 0;
+        quantity = 0;
+    }
+
+    public Cart(int id, User customer, double totalMoney, int quantity) {
+        this.id = id;
+        this.productList = new HashMap<>();
+        this.customer = customer;
+        this.totalMoney = totalMoney;
+        this.quantity = quantity;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public HashMap<String, Product> getProductList() {
+        return productList;
+    }
+
+    public void setProductList(HashMap<String, Product> productList) {
+        this.productList = productList;
+    }
+
+    public User getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(User customer) {
+        this.customer = customer;
+    }
+
+    public double getTotalMoney() {
+        return totalMoney;
+    }
+
+    public void setTotalMoney(double totalMoney) {
+        this.totalMoney = totalMoney;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
